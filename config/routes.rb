367bb66ff6185a462220931477eaddcb5b57888d
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-27 16:37:27 -0300

    feat: movie controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
