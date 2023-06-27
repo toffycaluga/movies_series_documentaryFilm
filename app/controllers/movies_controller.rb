3177aebe9c12613a758cd7190c79a2ed450a01e7
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-27 17:19:19 -0300

    feat:validations created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    # @movie= Movie.all
+    @movie= Movie.all
   end
 
   def new
   end
 
   def create
+    @movie= Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
 end
--- app/models/documentary_film.rb
+++ app/models/documentary_film.rb
@@ -1,2 +1,5 @@
 class DocumentaryFilm < ApplicationRecord
+    validates :name,presence: true
+    validates :synopsis,presence: true
+    validates :director,presence: true
 end
