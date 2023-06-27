b767a9784b9779bd719a79cf5b79c0805d708e12
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-27 17:43:25 -0300

    Revert "feat:error no solucionado"
    This reverts commit c6e987b04c7875429847a0ce8ac1ddc56135e2a8.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,9 @@
 class MoviesController < ApplicationController
-  @errors=[]
   def index
-    @errors
     @movie= Movie.all
   end
 
   def new
-    @errors
   end
 
   def create
@@ -14,13 +11,6 @@ class MoviesController < ApplicationController
     if @movie.save
       redirect_to movies_path
     else
-      @errors.push(
-        {
-          title:"Algo Salio mal",
-          description:"No se logro guardar en la base de datos"
-        }
-      )
-      puts @error
       render :new
     end
   end
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,5 +1,2 @@
 class Serie < ApplicationRecord
-    validates :name,presence: true
-    validates :synopsis,presence: true
-    validates :director,presence: true
 end
