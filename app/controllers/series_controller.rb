64b1569ed65437f1cadfd65c98c2fd680e9a003a
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-27 18:31:26 -0300

    feat:saved series in database, configured

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,10 +1,24 @@
 class SeriesController < ApplicationController
   def index
+    @serie=Serie.all
   end
 
   def new
+    #Captura los datos enviados en el formulario
+    @serie = Serie.new
   end
 
   def create
+    # Guardando peliculas en db
+    @serie= Serie.new(serie_params)
+    if @serie.save
+      redirect_to series_path
+    else
+      render :new
+    end
+
+  end
+  def serie_params
+    params.require(:serie).permit(:name,:synopsis,:director)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Rutas Post
   post "movies" =>"movies#create"
+  post "series" =>"series#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
